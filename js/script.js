325a5ef045208fe8deb95ad578d3b979df4d475f
Pablo de Andres <pablodeandresolano@gmail.com>
2025-02-06 15:57:52 +0100

    Serpiente

--- js/script.js
+++ js/script.js
@@ -2,21 +2,43 @@ $(document).ready(function () {
   $(".start").on("click", startGame);
 });
 
+let interval;
+let snakeTopPos = 250;
+let snakeLeftPos = 250;
+let snakeBody = [{ top: snakeTopPos, left: snakeLeftPos }];
+
 function startGame() {
-  var interval;
+  if (interval) {
+    clearInterval(interval);
+  }
+
+  spawnFood();
 
   $(".reset").on("click", () => {
     endGame(interval);
   });
 
-  console.log("start");
-  let x = 250;
-  let y = 250;
+  let snake = $(".snake");
+  let gameBoard = $(".gameBoard");
 
-  $(".snake").css("display", "block");
-  $(".snake").css("top", y);
-  $(".snake").css("left", x);
+  snakeTopPos = 250;
+  snakeLeftPos = 250;
+  snakeBody = [{ top: snakeTopPos, left: snakeLeftPos }];
 
+  snake.css({
+    display: "block",
+    top: snakeTopPos + "px",
+    left: snakeLeftPos + "px",
+  });
+
+  let boardWidth = gameBoard.width();
+  let boardHeight = gameBoard.height();
+  let snakeWidth = snake.width();
+  let snakeHeight = snake.height();
+
+  interval = setInterval(rightMove, 100);
+
+  $(document).off("keydown");
   $(document).on("keydown", (event) => {
     switch (event.key) {
       case "ArrowRight":
@@ -40,33 +62,95 @@ function startGame() {
         clearInterval(interval);
         interval = setInterval(downMove, 100);
         console.log(event.key);
-
         break;
     }
   });
 
   function rightMove() {
-    y += 10;
-    $(".snake").css("left", y);
+    if (snakeLeftPos + 10 + snakeWidth <= boardWidth) {
+      snakeLeftPos += 10;
+      snake.css("left", snakeLeftPos);
+      checkCollision();
+    } else {
+      endGame(interval);
+    }
   }
 
   function leftMove() {
-    y -= 10;
-    $(".snake").css("left", y);
+    if (snakeLeftPos - 10 >= 0) {
+      snakeLeftPos -= 10;
+      snake.css("left", snakeLeftPos);
+      checkCollision();
+    } else {
+      endGame(interval);
+    }
   }
 
   function upMove() {
-    x -= 10;
-    $(".snake").css("top", x);
+    if (snakeTopPos - 10 >= 0) {
+      snakeTopPos -= 10;
+      snake.css("top", snakeTopPos);
+      checkCollision();
+    } else {
+      endGame(interval);
+    }
   }
 
   function downMove() {
-    x += 10;
-    $(".snake").css("top", x);
+    if (snakeTopPos + 10 + snakeHeight <= boardHeight) {
+      snakeTopPos += 10;
+      snake.css("top", snakeTopPos);
+      checkCollision();
+    } else {
+      endGame(interval);
+    }
+  }
+}
+
+function spawnFood() {
+  let gameBoard = $(".gameBoard");
+  let food = $(".food");
+
+  let boardWidth = gameBoard.width();
+  let boardHeight = gameBoard.height();
+  let foodSize = food.width();
+
+  let leftFoodPos =
+    Math.floor(Math.random() * ((boardWidth - foodSize) / 10)) * 10;
+  let topFoodPos =
+    Math.floor(Math.random() * ((boardHeight - foodSize) / 10)) * 10;
+
+  food.css({
+    display: "block",
+    top: topFoodPos + "px",
+    left: leftFoodPos + "px",
+  });
+}
+
+function checkCollision() {
+  let food = $(".food");
+  let foodSize = food.width();
+  let foodTopPos = parseInt(food.css("top"));
+  let foodLeftPos = parseInt(food.css("left"));
+
+  // Verificar si la cabeza de la serpiente colisiona con la comida
+  if (
+    snakeTopPos < foodTopPos + foodSize && // la cabeza de la serpiente está por encima de la comida
+    snakeTopPos + 10 > foodTopPos && // la cabeza de la serpiente está por debajo de la comida
+    snakeLeftPos < foodLeftPos + foodSize && // la cabeza de la serpiente está a la izquierda de la comida
+    snakeLeftPos + 10 > foodLeftPos // la cabeza de la serpiente está a la derecha de la comida
+  ) {
+    // La serpiente ha comido la comida
+    spawnFood(); // Generamos una nueva comida
+    console.log("Colisión detectada, comida consumida");
   }
 }
 
 function endGame(interval) {
   clearInterval(interval);
-  startGame();
+  $(".snake").css({
+    display: "none",
+  });
+
+  alert("Game Over");
 }
